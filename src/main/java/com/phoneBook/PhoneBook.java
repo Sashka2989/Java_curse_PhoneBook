46a512c7c9d6563151a2d0dad1b15f7765b9d8ca
Sashka2989 <Sashu998@gmail.com>
2018-11-06 10:14:23 +0700

    Add lists in main

--- src/main/java/com/phoneBook/PhoneBook.java
+++ src/main/java/com/phoneBook/PhoneBook.java
@@ -8,10 +8,22 @@ public class PhoneBook{
 		UrFace u2 = new UrFace("TwoPers", "44051", "Ktshno", "192671648187");
 		System.out.println(u1.GetData());
 		System.out.println(u2.GetData());
+		
 		ArrayList <FizFace> l1 = new ArrayList <> ();
 		l1.add(new FizFace("SpisPersOne", "22989", "Pshkena", "89005553535"));
-		l1.add(new FizFace("SpisPersTwo", "24987", "Pshkena", "87005553535"));
-		l1.get();
+		l1.add(new FizFace("SpisPersTwo", "24987", "Pdads", "87005553535"));
+		
+		ArrayList <UrFace> l2 = new ArrayList <> ();
+		l2.add(new UrFace("SpisUrPersOne", "2801", "Psdfa", "768723"));
+		l2.add(new UrFace("SpisUrPersTwo", "280121", "Wesfna", "822345"));
+		System.out.println("Fiz Face list:");
+		System.out.println(l1.get(0).GetData());
+		System.out.println(l1.get(1).GetData());
+		
+		System.out.println("Ur Face list:");
+		System.out.println(l2.get(0).GetData());
+		System.out.println(l2.get(1).GetData());
+		
 	}
 	
 	
